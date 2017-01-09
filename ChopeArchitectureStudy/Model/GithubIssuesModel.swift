1d8c686496202c3cec6912522762d0cc64aaae27
Chope <yoonhg2002@gmail.com>
2017-01-10 01:53:55 +0900

    이슈목록 MVC -> MVP

--- ChopeArchitectureStudy/Model/GithubIssuesModel.swift
+++ ChopeArchitectureStudy/Model/GithubIssuesModel.swift
@@ -7,17 +7,19 @@ import Foundation
 import Alamofire
 import XCGLogger
 
-class IssuesModel {
-    static let ChangedNotification: Notification.Name = Notification.Name("githubIssuesChanged")
-
+class GithubIssuesModel: IssuesModel {
     let user: String
     let repo: String
 
-    private(set) var issues: [Issue] = []
+    var issues: [Issue] = []
 
     init(user: String, repo: String) {
         self.user = user
         self.repo = repo
+    }
+
+    class func notificationNameOfChangedIssues() -> Notification.Name {
+        return Notification.Name("githubIssuesChanged")
     }
 
     func load() {
@@ -31,7 +33,7 @@ class IssuesModel {
                             self.issues.append(Issue(json: issueJson))
                         }
 
-                        NotificationCenter.default.post(name: type(of: self).ChangedNotification, object: self)
+                        self.postNotification()
                     }
                 }
     }
